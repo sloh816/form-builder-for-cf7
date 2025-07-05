00eea11ec411882c99f8f55a047cac4ff994f6ef
Shannon Loh <shannon.loh816@gmail.com>
2025-07-05 14:21:24 +1000

    Move insert field button to a component

---
+++ src/components/InsertFieldAboveButton.vue
@@ -0,0 +1,23 @@
+<template>
+	<button
+		class="text-sm text-indigo-600 flex items-center gap-2 cursor-pointer opacity-0 hover:opacity-100 transition-all duration-200"
+		:style="{ width: 'calc(100% + 60px)' }"
+		@click="openFieldsModal(index)"
+	>
+		<span class="block h-[1px] bg-indigo-600 w-full"></span>
+		<span class="block text-nowrap bg-indigo-100 px-2 rounded">+ Insert field above</span>
+	</button>
+</template>
+
+<script setup lang="ts">
+import { inject } from "vue";
+
+const setAddFormFieldIndex = inject("setAddFormFieldIndex");
+function openFieldsModal(index: number | null) {
+	if (typeof setAddFormFieldIndex === "function") {
+		setAddFormFieldIndex(index); // Reset the index to null when opening the modal
+	}
+	const modal = document.querySelector(".fields-modal");
+	modal?.classList.remove("hidden");
+}
+</script>
--- src/components/formbuilder/TextInput.vue
+++ src/components/formbuilder/TextInput.vue
@@ -1,36 +1,63 @@
 <template>
-    <FormPreviewField>
-        <template #preview>
-            <label :data-field-id="props.id">
-                <span class="indiga-label">{{ props.label }}<span v-if="props.required">*</span></span>
+	<FormPreviewField>
+		<template #preview>
+			<label :data-field-id="props.id">
+				<span class="indiga-label"
+					>{{ props.label }}<span v-if="props.required">*</span></span
+				>
 
-                <span class="wpcf7-form-control-wrap" :data-name="props.id">
-                    <input size="40" maxlength="400" class="wpcf7-form-control wpcf7-text pointer-events-none" aria-invalid="false" value="" type="text" :name="props.id" :placeholder="props.placeholder" :id="props.id" disabled />
-                </span>
-            </label>
-        </template>
-        <template #options>
-            <FieldOptionsFormWrapper :id="props.id" fieldType="Text input">
-                <TextInput :id="`label-${props.id}`" label="Label" :value="props.label" :inputFunction="(value: any) => updateProps('label', value)" />
+				<span class="wpcf7-form-control-wrap" :data-name="props.id">
+					<input
+						size="40"
+						maxlength="400"
+						class="wpcf7-form-control wpcf7-text pointer-events-none"
+						aria-invalid="false"
+						value=""
+						type="text"
+						:name="props.id"
+						:placeholder="props.placeholder"
+						:id="props.id"
+					/>
+				</span>
+			</label>
+		</template>
+		<template #options>
+			<FieldOptionsFormWrapper :id="props.id" fieldType="Text input">
+				<TextInput
+					:id="`label-${props.id}`"
+					label="Label"
+					:value="props.label"
+					:inputFunction="(value: any) => updateProps('label', value)"
+				/>
 
-                <RadioButtons
-                    :id="`type-${props.id}`"
-                    label="Input type"
-                    :options="[
-                        { label: 'Name', isChecked: props.type === 'name', value: 'name' },
-                        { label: 'Email', isChecked: props.type === 'email', value: 'email' },
-                        { label: 'Phone', isChecked: props.type === 'phone', value: 'phone' },
-                        { label: 'Other', isChecked: props.type === 'text', value: 'text' },
-                    ]"
-                    :changeFunction="(value: any) => updateProps('type', value)"
-                />
+				<RadioButtons
+					:id="`type-${props.id}`"
+					label="Input type"
+					:options="[
+						{ label: 'Name', isChecked: props.type === 'name', value: 'name' },
+						{ label: 'Email', isChecked: props.type === 'email', value: 'email' },
+						{ label: 'Phone', isChecked: props.type === 'phone', value: 'phone' },
+						{ label: 'Other', isChecked: props.type === 'text', value: 'text' }
+					]"
+					:changeFunction="(value: any) => updateProps('type', value)"
+				/>
 
-                <TextInput :id="`placeholder-${props.id}`" label="Placeholder" :value="props.placeholder || ''" :inputFunction="(value: any) => updateProps('placeholder', value)" />
+				<TextInput
+					:id="`placeholder-${props.id}`"
+					label="Placeholder"
+					:value="props.placeholder || ''"
+					:inputFunction="(value: any) => updateProps('placeholder', value)"
+				/>
 
-                <Boolean :id="`required-${props.id}`" :isChecked="props.required" :changeFunction="(value: any) => updateProps('required', value)" label="Required" />
-            </FieldOptionsFormWrapper>
-        </template>
-    </FormPreviewField>
+				<Boolean
+					:id="`required-${props.id}`"
+					:isChecked="props.required"
+					:changeFunction="(value: any) => updateProps('required', value)"
+					label="Required"
+				/>
+			</FieldOptionsFormWrapper>
+		</template>
+	</FormPreviewField>
 </template>
 
 <script setup lang="ts">
@@ -42,11 +69,11 @@ import FormPreviewField from "../../components/FormPreviewField.vue";
 import { inject } from "vue";
 
 interface Props {
-    label: string;
-    type?: string;
-    placeholder?: string;
-    required?: true | false;
-    id: string;
+	label: string;
+	type?: string;
+	placeholder?: string;
+	required?: true | false;
+	id: string;
 }
 
 const props = defineProps<Props>();
@@ -54,8 +81,8 @@ const props = defineProps<Props>();
 const updateFormField = inject<Function>("updateFormField");
 
 function updateProps(propKey: keyof Props, value: any) {
-    const newProps = { ...props } as Record<string, any>;
-    newProps[propKey] = value;
-    updateFormField?.(props.id, newProps);
+	const newProps = { ...props } as Record<string, any>;
+	newProps[propKey] = value;
+	updateFormField?.(props.id, newProps);
 }
 </script>
